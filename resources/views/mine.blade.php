328973f67cf4945d260f6f9b066044baefa11a74
dreysongz <aduramimo@gmail.com>
2020-06-24 15:15:53 +0000

    init

---
+++ resources/views/mine.blade.php
@@ -0,0 +1,24 @@
+
+<!doctype html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=5ef0f923b61c220011098161&product=inline-share-buttons" async="async"></script>
+
+  <title> Pizza Ordering App</title>
+    <link href="https://fonts.googleapis.com/css2?family=Zilla+Slab:wght@300&display=swap" rel="stylesheet">
+   <style type="text/css">
+   	body{
+   	 font-size: 22px;
+    font-family: 'Zilla Slab', serif;
+      	}
+   </style>
+</head>
+
+<body>
+<center><span><p> Welcome to the Pizza Odrering App!!</span><br>
+<div class="sharethis-inline-share-buttons"></div>
+
+<p>This is Pizza from {{$type}}. It costs {{$price}} and its been sold at {{$location}}</p></center>
+
+</body>
+</html>
